1865f33ccff1cbc54b1ce63ed0a84df1b846fcc1
Daniele Nanni Cirulli <danielenannicirullu00@gmail.com>
2024-01-19 18:02:15 +0100

    Nuovi commenti

--- networkDistribuited.py
+++ networkDistribuited.py
@@ -9,10 +9,10 @@ from mininet.link import Intf
 class Router(Node):
    def config(self, **params):
       super(Router, self).config(**params)
-      self.cmd("sysctl net.ipv4.ip_forward=1")
+      self.cmd("sysctl net.ipv4.ip_forward=1") #comando per abilitare ip forward
    
    def terminate(self):
-      self.cmd("sysctl net.ipv4.ip_forward=0")
+      self.cmd("sysctl net.ipv4.ip_forward=0")  #comando per disabilitare ip forward
       super(Router, self).terminate()
 
 #Creazione topologia di rete personalizzata
@@ -20,7 +20,7 @@ class MyTopologia(Topo):
    def build(self, **_kwargs):
       r0 = self.addNode("r0", cls=Router) #aggiunta nodo router
       switch1 = self.addSwitch("s1")
-      #Aggiunta di 3 host nella prima rete con indirizzo 192.0.0.0/24
+      #Aggiunta di 3 host e assegnazione ip nella prima rete con indirizzo 192.0.0.0/24
       h1 = self.addHost("h1", ip="10.0.0.10/24", defaultRoute="via 10.0.0.1")
       h2 = self.addHost("h2", ip="10.0.0.11/24", defaultRoute="via 10.0.0.1")
       h3 = self.addHost("h3", ip="10.0.0.12/24", defaultRoute="via 10.0.0.1")
@@ -29,7 +29,7 @@ class MyTopologia(Topo):
       self.addLink(h2, switch1)
       self.addLink(h3, switch1)
       
-      #Aggiunta di 3 host alla second rete con indirizzo 192.168.0.0/24
+      #Aggiunta di 3 host e assegnazione ip nella seconda rete con indirizzo 192.168.0.0/24
       switch2 = self.addSwitch("s2")
       h3 = self.addHost("h3", ip="192.168.0.13/24", defaultRoute="via 192.168.0.1")
       h4 = self.addHost("h4", ip="192.168.0.14/24", defaultRoute="via 192.168.0.1")
@@ -39,19 +39,21 @@ class MyTopologia(Topo):
       self.addLink(h4, switch2)
       self.addLink(h5, switch1)
 
+      #Collegamento tra il router e i due switch attraverso interfaccia
       self.addLink(r0, switch1, intfName1="r0-eth1", params1={"ip": "10.0.0.1/24"})
       self.addLink(r0, switch2, intfName1="r0-eth2", params1={"ip": "192.168.0.1/24"})
    
 def run():
-   topo = MyTopologia()
-   net = Mininet(topo=topo, waitConnected=True)
+   topo = MyTopologia() #istanziamento topologia di rete personalizzata
+   net = Mininet(topo=topo, waitConnected=True) 
    net.start()
    router = net.getNodeByName("r0")
-   _intf0 = Intf("enp0s3", node=router)
-   router.cmd('dhclient enp0s3')
-   router.cmd("ip r0-eth1 10.0.0.1 netmask 255.255.255.0")
-   router.cmd("ip r0-eth2 192.168.0.1 netmask 255.255.255.0")
-   router.cmd("iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE")
+   _intf0 = Intf("enp0s3", node=router) #interfaccia enp0s3 configurata come router
+   router.cmd('dhclient enp0s3') #assegna tramite dhclinet indirizzo (?)
+   router.cmd("ifconfig r0-eth1 10.0.0.1 netmask 255.255.255.0")
+   router.cmd("ifconfig 192.168.0.1 netmask 255.255.255.0")
+   router.cmd("iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE") #regole di NAT per router
+   
    CLI(net)
    net.stop()
 
