609faacdec6538f7282d537f8ce04867729d80a0
Daniele Nanni Cirulli <daniele.nannicirulli@studenti.unipg.it>
2024-04-09 15:55:36 +0200

    edit

--- networkDistribuited.py
+++ networkDistribuited.py
@@ -18,8 +18,8 @@ class Router(Node):
 #Creazione topologia di rete personalizzata
 class MyTopologia(Topo):
    def build(self, **_kwargs):
-      r0 = self.addNode("r0", cls=Router) #aggiunta nodo router
-      switch1 = self.addSwitch("s1")
+      r0 = self.addNode("r0", cls=Router) #Aggiunta del router
+      switch1 = self.addSwitch("s1")#Aggiunta del primo switch
       #Aggiunta di 3 host e assegnazione ip nella prima subnet con indirizzo 10.0.0.0/24
       h1 = self.addHost("h1", ip="10.0.0.10/24", defaultRoute="via 10.0.0.1")
       h2 = self.addHost("h2", ip="10.0.0.11/24", defaultRoute="via 10.0.0.1")
@@ -30,7 +30,7 @@ class MyTopologia(Topo):
       self.addLink(h3, switch1)
       
       #Aggiunta di 3 host e assegnazione ip nella seconda subnet con indirizzo 192.168.0.0/24
-      switch2 = self.addSwitch("s2")
+      switch2 = self.addSwitch("s2")#Aggiunta del secondo switch
       h4 = self.addHost("h4", ip="192.168.0.13/24", defaultRoute="via 192.168.0.1")
       h5 = self.addHost("h5", ip="192.168.0.14/24", defaultRoute="via 192.168.0.1")
       h6 = self.addHost("h6", ip="192.168.0.15/24", defaultRoute="via 192.168.0.1")
@@ -40,7 +40,7 @@ class MyTopologia(Topo):
       self.addLink(h6, switch2)
 
       #Collegamento tra il router e i due switch attraverso interfaccia
-      self.addLink(r0, switch1, intfName1="r0-eth1", params1={"ip": "10.0.0.1/24"})
+      self.addLink(r0, switch1, intfName1="r0-eth1", params1={"ip": "10.0.0.1/24"}) 
       self.addLink(r0, switch2, intfName1="r0-eth2", params1={"ip": "192.168.0.1/24"})
    
 def run():
@@ -49,10 +49,10 @@ def run():
    net.start()
    router = net.getNodeByName("r0")
    _intf0 = Intf("enp0s3", node=router) #interfaccia enp0s3 configurata come router
-   router.cmd('dhclient enp0s3') #assegna tramite dhclinet indirizzo (?)
-   router.cmd("ifconfig r0-eth1 10.0.0.1 netmask 255.255.255.0")
-   router.cmd("ifconfig 192.168.0.1 netmask 255.255.255.0")
-   router.cmd("iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE") #regole di NAT per router
+   router.cmd('dhclient enp0s3') #assegna tramite dhcp indirizzo ip
+   router.cmd("ifconfig r0-eth1 10.0.0.1 netmask 255.255.255.0") #configurazione ip per interfaccia r0-eth1
+   router.cmd("ifconfig 192.168.0.1 netmask 255.255.255.0") #configurazione ip per interfaccia r0-eth2
+   router.cmd("iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE") #regola per il NAT
    
    CLI(net)
    net.stop()
